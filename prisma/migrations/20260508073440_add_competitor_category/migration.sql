eb7fb3f4980a62fc7e94669d81178e6694bf6cf6
kubar29 <kubarusek29@gmail.com>
2026-05-08 09:55:41 +0200

    migracja competitor do db

---
+++ prisma/migrations/20260508073440_add_competitor_category/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Competitor" ADD COLUMN     "category" TEXT;
--- src/controllers/competitor.controller.js
+++ src/controllers/competitor.controller.js
@@ -1,31 +1,35 @@
-let competitors = [];
+const prisma = require('../prisma/client');
 
-exports.getAllCompetitors = (req, res) => {
+exports.getAllCompetitors = async (req, res) => {
+    const competitors = await prisma.competitor.findMany();
     res.json(competitors);
 };
 
-exports.createCompetitor = (req, res) => {
+exports.createCompetitor = async (req, res) => {
     const { name, surname, category } = req.body;
 
-    const newCompetitor = {
-        id: Date.now(),
-        name,
-        surname,
-        category
-    };
-
-    competitors.push(newCompetitor);
+    const newCompetitor = await prisma.competitor.create({
+        data: {
+            name,
+            surname,
+            category: category || null
+        }
+    });
 
     res.status(201).json(newCompetitor);
 };
 
-exports.getCompetitorById = (req, res) => {
-    const id = parseInt(req.params.id);
+exports.getCompetitorById = async (req, res) => {
+    const id = Number(req.params.id);
 
-    const competitor = competitors.find(c => c.id === id);
+    const competitor = await prisma.competitor.findUnique({
+        where: { id }
+    });
 
     if (!competitor) {
-        return res.status(404).json({ message: "Nie znaleziono zawodnika"});
+        return res.status(404).json({ 
+            message: 'Nie znaleziono zawodnika'
+        });
     }
 
     res.json(competitor);
